c44ce02f96c3cc059552f2f47c1f5a7ecbc985b2
AlexanderGullen <developer@alexmgullen.ca>
2025-07-05 09:16:35 -0400

    fix: updated location of audio file

--- audio/src/visual.ts
+++ audio/src/visual.ts
@@ -13,34 +13,101 @@ import { VisualFormattingSettingsModel } from "./settings";
 
 export class Visual implements IVisual {
   private target: d3.Selection<any, any, any, any>;
+
   private visualUpdateOptions: powerbi.extensibility.visual.VisualConstructorOptions;
+
   private formattingSettings: VisualFormattingSettingsModel;
   private formattingSettingsService: FormattingSettingsService;
 
+  private localizationManager: powerbi.extensibility.ILocalizationManager;
+
+  private audio: HTMLAudioElement;
+  private audioContext: AudioContext;
+  private track: MediaElementAudioSourceNode;
+
+  private gainNode: GainNode;
+
   constructor(options: powerbi.extensibility.visual.VisualConstructorOptions) {
     try {
+      this.localizationManager = options.host.createLocalizationManager();
       this.target = d3.select(options.element);
       this.visualUpdateOptions = options;
       this.formattingSettingsService = new FormattingSettingsService();
+
       if (document) {
-        let audio = document.createElement("audio");
-        audio.controls = true;
+        this.audio = document.createElement("audio");
+        this.audio.src = "https://grognard.ca/assets/data/audio.wav";
+        this.audio.crossOrigin = "anonymous"
+        
+        this.audioContext = new AudioContext()
+        
+        this.track = this.audioContext.createMediaElementSource(this.audio)
 
-        let source = document.createElement("source");
-        source.src = "https://grognard.ca/assets/data/output.wav";
-        source.type = "audio/wav";
+        this.gainNode = this.audioContext.createGain();
+        this.track.connect(this.gainNode).connect(this.audioContext.destination)
 
-        audio.append(source);
+        this.target.append(() => this.audio);
 
-        this.target.append(() => audio);
+        let message = document.createElement("p")
+        
+        message.innerText = `${this.localizationManager.getDisplayName(
+        "displayMessage"
+        )}`
+
+        message.innerHTML = `<div class="tooltip">
+  <p>A lot can happen in 10 years…</p>
+  <p>try moving the slicer to see how much</p>
+  <svg role="presentation" viewbox="0 0 12 16">
+<filter id="shadow" color-interpolation-filters="sRGB">
+  <feDropShadow dx="1" dy="0" stdDeviation="0.5" flood-opacity="0.6"/>
+  </filter>
+  <path d="M2 1L8 5L2 9" filter="url(#shadow)"/>
+</svg>
+</div>`
+
+        this.target.append(() => message)
       }
     } catch (e) {
-      console.log("Constructor error: ", e);
+      this.visualUpdateOptions.host.displayWarningIcon("Construction Error","Failed to build loudspeaker")
     }
-    console.log("target: ", this.target);
   }
 
-  public update(options: VisualUpdateOptions) { }
+  public update(options: VisualUpdateOptions) {
+
+    try{
+
+      var volume: number = parseFloat(options.dataViews[0].single.value.toString())
+
+      if (volume > 100) {
+          volume = 100
+      }
+
+      //use linear mapping to adjust the volume to a range between 0 and 2
+      this.gainNode.gain.value = 0//volume * 0.02
+    
+      this.play()
+
+      this.audio.play()
+
+    } catch (e){
+      this.visualUpdateOptions.host.displayWarningIcon("Runtime Error","Something went wrong running loudspeaker")
+    }
+
+  }
+
+  public play(){
+
+    if(this.gainNode.gain.value === 0){
+      this.audioContext.suspend()
+    }else if(this.audioContext.state === "suspended"){
+      this.audioContext.resume()
+    }else if(this.audioContext.state === "running"){
+
+    }else{
+      this.audio.play()
+    }
+    
+  }
 
   /**
    * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
